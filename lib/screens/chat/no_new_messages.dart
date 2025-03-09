ab97a4acc5edd445c4348af5646838dccc9b9b2d
Ahmad Mahdaly <ahmed.mahdaly86@gmail.com>
2025-03-10 01:33:11 +0200

    Add NoNewMessages component for displaying no new requests in chat

---
+++ lib/screens/chat/no_new_messages.dart
@@ -0,0 +1,39 @@
+import 'package:biblio/animations/animate_do.dart';
+import 'package:biblio/utils/constants/colors_constants.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class NoNewMessages extends StatelessWidget {
+  const NoNewMessages({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomFadeInUp(
+      duration: 300,
+      child: Container(
+        alignment: Alignment.center,
+        child: Column(
+          spacing: 10.sp,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.email_outlined,
+              size: 46.sp,
+              color: kBorderColor,
+            ),
+            Text(
+              'لا توجد طلبات جديدة',
+              style: TextStyle(
+                color: kTextShadowColor,
+                fontSize: 16.sp,
+                fontWeight: FontWeight.w900,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
